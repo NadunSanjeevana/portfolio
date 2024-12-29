3b9cec5018957a685fadbb2b6a1e46461c7f3369
Nadun Sanjeevana <108867149+NadunSanjeevana@users.noreply.github.com>
2024-12-30 00:09:15 +0530

    fixed

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -13,12 +13,12 @@ import MagicButton from "../MagicButton";
 
 import dynamic from "next/dynamic";
 
-const BackgroundGradientAnimation = dynamic(
-  () => import("./GradientBg").then((m) => m.BackgroundGradientAnimation),
-  {
-    ssr: false,
-  }
-);
+// const BackgroundGradientAnimation = dynamic(
+//   () => import("./GradientBg").then((m) => m.BackgroundGradientAnimation),
+//   {
+//     ssr: false,
+//   }
+// );
 
 export const BentoGrid = ({
   className,
@@ -122,9 +122,9 @@ export const BentoGridItem = ({
         </div>
         {id === 6 && (
           // add background animation , remove the p tag
-          <BackgroundGradientAnimation>
-            <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
-          </BackgroundGradientAnimation>
+          // <BackgroundGradientAnimation>
+          <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
+          // </BackgroundGradientAnimation>
         )}
 
         <div
